6eb58631154eb8f5cf551659456cc0e3c49a6cfe
Artem Kopanev <artemkopanev757@gmail.com>
2025-04-20 19:33:46 +0300

    init

---
+++ src/main/java/ru/kopanev/spring/feature_1/Dog.java
@@ -0,0 +1,8 @@
+package ru.kopanev.spring.feature_1;
+
+public class Dog extends Animal {
+
+    public Dog(String name, int runLimitation, int swimLimitation) {
+        super(name, runLimitation, swimLimitation);
+    }
+}
